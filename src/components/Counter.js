3281f6502a395f8c3806dd31af1b0c5db60b1df5
bharatsuchith97 <bharatsuchith97@gmail.com>
2021-12-19 15:28:01 +0530

    Added an input to set the value of counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,33 +1,35 @@
-import React,{useState} from 'react'
-import Controllers from "./Controllers"
-import {setCount} from '../actions'
-import { useSelector,useDispatch } from 'react-redux'
+import React, { useState } from 'react'
+import { setCount } from '../actions'
+import { useSelector, useDispatch } from 'react-redux'
+import { increaseCount, decreaseCount } from '../actions'
 
 const Counter = (props) => {
     const dispatch = useDispatch();
-    const [value,setValue] = useState(props.initialCount);
-    const [error,setError] = useState('');
-
     const count = useSelector(state => state.count);
-    const setValueChange = (e) =>{
+
+    const [value, setValue] = useState(props.initialCount);
+    const [error, setError] = useState('');
+
+    const setValueChange = (e) => {
         setValue(parseInt(e.target.value))
     }
-    const setCounter = () =>{
-        if(Number.isInteger(value)){
+    const setCounter = () => {
+        if (Number.isInteger(value)) {
             setError('');
             dispatch(setCount(value));
         }
-        else{
-            setError('Value must be an integer')   
+        else {
+            setError('Value must be an integer')
         }
     }
     return (
         <>
             <input type="text" onChange={setValueChange} /><button onClick={setCounter}>Set Counter</button>
-            <br/>
-            {error!=='' && <span style={{color:"red"}}>{error}</span>}
+            <br />
+            {error !== '' && <span style={{ color: "red" }}>{error}</span>}
             <h1>{count}</h1>
-            <Controllers />
+            <button id="minusBtn" onClick={() => dispatch(decreaseCount(count))}>-</button>
+            <button id="plusBtn" onClick={() => dispatch(increaseCount(count))}>+</button>
         </>
     )
 }
