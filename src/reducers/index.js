605a9bc49af7e1b3c0d6b819405728307b58fe97
bharatsuchith97 <bharatsuchith97@gmail.com>
2021-12-19 14:14:26 +0530

    Initial push

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+const countReducer = (state = {count: 0}, action) => {
+    switch(action.type) {
+      case "INCREASE_COUNT":
+        return {...state, count: action.payload}
+      case "DECREASE_COUNT":
+        return {...state, count: action.payload}
+      default:
+        return state
+    }
+  }
+  
+  export default countReducer
